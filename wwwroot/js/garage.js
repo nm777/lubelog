fcb50e3571a03041976c2474085bfa54847b4ee9
Nate Morris <njmorris777@gmail.com>
2025-06-23 07:25:15 -0500

    try global js variable

--- wwwroot/js/garage.js
+++ wwwroot/js/garage.js
@@ -15,7 +15,7 @@ function hideAddVehicleModal() {
 }
 //refreshable function to reload Garage PartialView
 function loadGarage() {
-    $.get(getGlobalConfig().pathBase + '/Home/Garage', function (data) {
+    $.get(`${pathBase}/Home/Garage`, function (data) {
         $("#garageContainer").html(data);
         loadSettings();
         bindTabEvent();
