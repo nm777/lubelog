65b1bc8068fe82e95b2e19f784ec5c8bc85380b4
Nate Morris <njmorris777@gmail.com>
2025-06-23 07:27:39 -0500

    try again with fixed typos

--- wwwroot/js/garage.js
+++ wwwroot/js/garage.js
@@ -15,7 +15,7 @@ function hideAddVehicleModal() {
 }
 //refreshable function to reload Garage PartialView
 function loadGarage() {
-    $.get(`${pathBase}/Home/Garage`, function (data) {
+    $.get(`${window.pathBase}/Home/Garage`, function (data) {
         $("#garageContainer").html(data);
         loadSettings();
         bindTabEvent();
